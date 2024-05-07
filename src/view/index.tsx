dd4c90636099fb23022323d8b3791b5500468c38
Vlad Chmara <chmaravlad5@gmail.com>
2024-05-07 22:24:29 +0300

    init commit installed react router and added main page with structure

---
+++ src/view/index.tsx
@@ -0,0 +1,20 @@
+import './index.css';
+
+import { Routes } from './routes';
+
+function App() {
+  // return (
+    // <ThemeProvider theme = { defaultTheme }>
+    //         <GlobalStyles />
+    //         <AppContainer>
+    //             <Routes />
+    //         </AppContainer>
+    //     </ThemeProvider>
+  // );
+
+  return (
+    <Routes />
+  )
+}
+
+export default App;
---
+++ src/view/pages/HomePage/index.tsx
@@ -0,0 +1,29 @@
+// Core
+import React, { FC } from 'react';
+
+// Components
+// import { ErrorBoundary } from '../../components';
+
+// Elements
+// import { HelloBurst } from '../../elements';
+
+// Styles
+// import { Container } from './styles';
+
+const Root: FC = () => {
+    return (
+        // <Container>
+            <h1>Hello</h1>
+        // </Container>
+    );
+};
+
+export default () => (
+        <Root />
+    
+);
+{/* export default () => (
+    <ErrorBoundary>
+        <Root />
+    </ErrorBoundary>
+) */}
