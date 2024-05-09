39006f467f9360d1bba46e5ee174728b1e5f9106
Vlad Chmara <chmaravlad5@gmail.com>
2024-05-09 22:19:53 +0300

    installed RTK Query, added logic for Login with query to sewrver

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter,
 } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { store } from './store';
 
 
 const root = ReactDOM.createRoot(
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/view/pages/AuthPage/index.tsx
+++ src/view/pages/AuthPage/index.tsx
@@ -1,9 +1,20 @@
 // Core
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import { ErrorBoundary } from '../../Components/ErrorBoundary';
+import { useLoginMutation } from '../../../services/authApi';
 
 const Root: FC = () => {
+    const [login, data] = useLoginMutation()
+    const {isLoading, isError} = data
+    
+    useEffect(()=>{
+        login({email: 'john@email.com', password: 'password'})
+    },[])
+
+    if(isLoading) return <h1>Loading...</h1>
+    if(isError) return <h1>Error...</h1>
+
     return (
             <h1>Auth Page</h1>
     );
