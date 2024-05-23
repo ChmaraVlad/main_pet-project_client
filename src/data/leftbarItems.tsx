68d24437126991ce528118534a15bad3e419c000
Vlad Chmara <chmaravlad5@gmail.com>
2024-05-23 19:37:18 +0300

    updated routes added Products page and main styles for app

--- src/data/leftbarItems.tsx
+++ src/data/leftbarItems.tsx
@@ -3,14 +3,15 @@ import AccountPage from "../view/pages/AccountPage";
 import AdminPage from "../view/pages/AdminPage";
 import AuthPage from "../view/pages/AuthPage";
 import HomePage from "../view/pages/HomePage";
+import ProductsPage from "../view/pages/ProductsPage";
 
 export const SIDEBAR_DATA = [
   {
-    id: 1,
-    name: "account",
-    path: "/account",
-    icon: <UserIcon />,
-    component: <AccountPage />,
+    id: 3,
+    name: "authentication",
+    path: "/authentication",
+    icon: <AuthIcon />,
+    component: <AuthPage />,
   },
   {
     id: 2,
@@ -20,11 +21,18 @@ export const SIDEBAR_DATA = [
     component: <HomePage />,
   },
   {
-    id: 3,
-    name: "authentication",
-    path: "/authentication",
-    icon: <AuthIcon />,
-    component: <AuthPage />,
+    id: 1,
+    name: "account",
+    path: "/account",
+    icon: <UserIcon />,
+    component: <AccountPage />,
+  },
+  {
+    id: 1,
+    name: "Products",
+    path: "/products",
+    icon: <UserIcon />,
+    component: <ProductsPage />,
   },
   {
     id: 3,
